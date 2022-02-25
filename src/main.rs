6abbd7c70bc9d629361c1ef8fe2d86c8f9a9238b
Daniel Schwartz <me@dansdomain.net>
2022-02-25 10:14:54 +0200

    Remove the secret number print line

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(0..101);
 
-    println!("The secret number is {}", secret_number);
-
     loop {
         println!("Please input your guess.");
 
